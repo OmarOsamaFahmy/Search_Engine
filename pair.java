a58c03ed7f2662301d445ab0a4b03a8873dee755
yomnaa <yomnaismail95@gmail.com>
2017-03-25 21:52:49 +0200

    Utilities

---
+++ pair.java
@@ -0,0 +1,18 @@
+package com.company;
+
+/**
+ * Created by yomna on 3/15/17.
+ */
+public class pair<l,r> {
+    public l first=null;
+    public r second=null;
+    public pair()
+    {
+
+    }
+    public pair(l f,r s)
+    {
+        first=f;
+        second=s;
+    }
+}
